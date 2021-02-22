d48cf48e3061d813abf9a0ade171afabd934a89b
Fernando Lomeli <fercho0.fpl@gmail.com>
2021-02-22 13:10:12 -0600

    Scroll animations added

--- js/mobileMenu.js
+++ js/mobileMenu.js
@@ -1,16 +1,27 @@
-(function () {
-  const mobileMenu = document.querySelector(".mobileMenu");
-  const hamburgerIcon = document.querySelectorAll(".hamburgerMenuIcon");
-  const body = document.querySelector("body");
-  for (let i = 0; i <= hamburgerIcon.length; i++) {
-    const bodyPositionStyle = getComputedStyle(body);
-    hamburgerIcon[i].addEventListener("click", () => {
-      mobileMenu.classList.toggle("mobileMenu--active");
-      if (bodyPositionStyle.position === "fixed") {
-        body.style.position = "relative";
-      } else {
-        body.style.position = "fixed";
-      }
-    });
+const mobileMenu = document.querySelector(".mobileMenu");
+const hamburgerIcon = document.querySelectorAll(".hamburgerMenuIcon");
+const body = document.querySelector("body");
+const menuItems = document.querySelectorAll(".mobileMenu__nav a");
+
+let isActive = false;
+for (let i = 0; i < menuItems.length; i++) {
+  menuItems[i].addEventListener("click", () => {
+    handleClickMobileMenu();
+  });
+}
+
+for (let i = 0; i <= hamburgerIcon.length; i++) {
+  hamburgerIcon[i].addEventListener("click", () => {
+    handleClickMobileMenu();
+  });
+}
+
+function handleClickMobileMenu() {
+  isActive = !isActive;
+  mobileMenu.classList.toggle("mobileMenu--active");
+  if (isActive) {
+    body.style.position = "fixed";
+  } else {
+    body.style.position = "relative";
   }
-})();
+}

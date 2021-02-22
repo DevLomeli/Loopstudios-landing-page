b19b4f7a850d2e012b4333efc32e90ed7dbb0690
Fernando Lomeli <fercho0.fpl@gmail.com>
2021-02-21 20:29:02 -0600

    First commit: mobile first completed

---
+++ js/mobileMenu.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.querySelector(".mobileMenu");
+const hamburgerIcon = document.querySelectorAll(".hamburgerMenuIcon");
+const body = document.querySelector("body");
+for (let i = 0; i <= hamburgerIcon.length; i++) {
+  const bodyPositionStyle = getComputedStyle(body);
+  hamburgerIcon[i].addEventListener("click", () => {
+    mobileMenu.classList.toggle("mobileMenu--active");
+    if (bodyPositionStyle.position === "fixed") {
+      body.style.position = "relative";
+    } else {
+      body.style.position = "fixed";
+    }
+  });
+}
